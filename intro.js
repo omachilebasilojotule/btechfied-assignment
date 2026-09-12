250d569fffe08e209fe126446a894b3c6a2dee6e
OMACHILE BASIL OJOTULE <omachilebasil@gmail.com>
2026-09-12 20:20:22 +0100

    Add files via upload
    week1 assignment

---
+++ intro.js
@@ -0,0 +1,9 @@
+// My Introduction
+
+let name = "Omachile";
+let age = 20;
+let favoriteProgrammingLanguage = "Python";
+
+console.log("My name is " + name);
+console.log("I am " + age + " years old");
+console.log("My favorite programming language is " + favoriteProgrammingLanguage);
